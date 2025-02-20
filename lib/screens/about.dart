e1ddaa358523422fe4545b3f3ee3e91470dd350b
Ronelle Cudjoe <ronnycudjoe9@gmail.com>
2025-02-20 23:25:04 +0000

    added about page and information

--- lib/screens/about.dart
+++ lib/screens/about.dart
@@ -7,11 +7,42 @@ class About extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('About')),
-      body: const Center(
-        child: Text(
-          'Contact Management App\nVersion 1.0.0\nDeveloped using Flutter!',
-          textAlign: TextAlign.center,
-          style: TextStyle(fontSize: 18),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Center(
+              child: Icon(Icons.contact_page, size: 80, color: Colors.blue),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              "Contact Management App",
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+            const Text("Version 1.0.0", style: TextStyle(fontSize: 16)),
+            const SizedBox(height: 20),
+            const Text(
+              "Developer Information",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const Text("Name: Ronelle Cudjoe"),
+            const Text("Student ID: 50942026"),
+            const SizedBox(height: 20),
+            const Text(
+              "About the App",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const Text(
+              "This a contact management app that allows users to manage"
+              "their contacts efficiently. It has features such as creating,"
+              "reading, updating and deleting contact information details."
+              "It uses an API to read from and store the data."
+            ),
+            const SizedBox(height: 20),
+            
+
+          ],
         ),
       ),
     );
